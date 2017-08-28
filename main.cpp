98819b826cbd041b2e552ece0336971f0d31859d
Aurélien Gâteau <mail@agateau.com>
2017-08-28 19:05:20 +0200

    Define application icon

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "MainWindow.h"
+
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     app.setOrganizationName("agateau.com");
     app.setApplicationName("nanonote");
+    app.setWindowIcon(QIcon(":/appicon.svg"));
     MainWindow window;
     window.show();
 
