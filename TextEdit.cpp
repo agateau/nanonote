7a9636129de13897b2f367cc21afe39125a1ad66
Aurelien Gateau <mail@agateau.com>
2018-02-02 09:47:28 +0100

    Keep indentation when starting a new line

--- TextEdit.cpp
+++ TextEdit.cpp
@@ -1,9 +1,22 @@
 #include "TextEdit.h"
 
 #include <QContextMenuEvent>
+#include <QDebug>
 #include <QMenu>
+#include <QTextBlock>
 
 static const int INDENT_SIZE = 4;
+
+static QString getIndentation(const QString &line)
+{
+    int idx;
+    for (idx = 0; idx < line.length(); ++idx) {
+        if (line[idx] != ' ') {
+            break;
+        }
+    }
+    return line.left(idx);
+}
 
 TextEdit::TextEdit(QWidget *parent)
     : QTextEdit(parent)
@@ -25,6 +38,10 @@ void TextEdit::keyPressEvent(QKeyEvent *event)
 {
     if (event->key() == Qt::Key_Tab) {
         insertPlainText(QString(INDENT_SIZE, ' '));
+        event->accept();
+    } else if (event->key() == Qt::Key_Enter || event->key() == Qt::Key_Return) {
+        QString indentation = getIndentation(textCursor().block().text());
+        insertPlainText('\n' + indentation);
         event->accept();
     } else {
         QTextEdit::keyPressEvent(event);
