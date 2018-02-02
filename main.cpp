08380523c2796548a82eaff08a5e9caa5ffca513
Aurelien Gateau <mail@agateau.com>
2018-02-02 18:05:14 +0100

    Add .desktop

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     app.setOrganizationName("agateau.com");
     app.setApplicationName("nanonote");
-    app.setWindowIcon(QIcon(":/appicon.svg"));
+    app.setWindowIcon(QIcon(":/icons/appicon.svg"));
     MainWindow window;
     window.show();
 
