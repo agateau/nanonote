5d2bd7bfa5fc0d8e2279e298d7261f2e9f392d74
Aurelien Gateau <mail@agateau.com>
2019-01-14 23:27:32 +0100

    Start adding translations

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include "MainWindow.h"
 
+#include <QCoreApplication>
+#include <QDebug>
 #include <QIcon>
+#include <QLocale>
+#include <QTranslator>
 
 #include <singleapplication.h>
 
 #include "Config.h"
+
+static void loadTranslations(QObject *parent)
+{
+    auto appDir = QCoreApplication::applicationDirPath();
+    auto translator = new QTranslator(parent);
+    QLocale locale;
+    for (const auto &dir : {appDir, }) {
+        if (translator->load(locale, QCoreApplication::applicationName(), "_", dir)) {
+            QCoreApplication::installTranslator(translator);
+            return;
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -14,6 +31,8 @@ int main(int argc, char *argv[])
     app.setApplicationName("nanonote");
     app.setApplicationVersion(NANONOTE_VERSION);
     app.setWindowIcon(QIcon(":/icons/appicon.svg"));
+
+    loadTranslations(&app);
 
     MainWindow window;
 
---
+++ src/translations/nanonote_fr.ts
@@ -0,0 +1,81 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en_US">
+<context>
+    <name>IndentExtension</name>
+    <message>
+        <location filename="../IndentExtension.cpp" line="51"/>
+        <source>Indent</source>
+        <translation>Indenter</translation>
+    </message>
+    <message>
+        <location filename="../IndentExtension.cpp" line="58"/>
+        <source>Unindent</source>
+        <translation>Désindenter</translation>
+    </message>
+</context>
+<context>
+    <name>LinkExtension</name>
+    <message>
+        <location filename="../LinkExtension.cpp" line="27"/>
+        <source>Copy link address</source>
+        <translation>Copier l&apos;adresse du lien</translation>
+    </message>
+    <message>
+        <location filename="../LinkExtension.cpp" line="32"/>
+        <source>Open link</source>
+        <translation>Ouvrir le lien</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../MainWindow.cpp" line="91"/>
+        <source>Increase Font Size</source>
+        <translation>Augmenter la taille du texte</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="94"/>
+        <source>Decrease Font Size</source>
+        <translation>Réduire la taille du texte</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="103"/>
+        <source>Always on Top</source>
+        <translation>Toujours visible</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="108"/>
+        <source>Settings...</source>
+        <translation>Configuration...</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../SettingsDialog.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Configuration</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="24"/>
+        <source>Appearance</source>
+        <translation>Apparence</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="30"/>
+        <source>Font family:</source>
+        <translation>Police :</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="42"/>
+        <source>Size:</source>
+        <translation>Taille :</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="68"/>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+</context>
+</TS>
