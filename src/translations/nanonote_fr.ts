9a4075589eb7f2d149f24ecae69b9348d8e6f8fe
Aurelien Gateau <mail@agateau.com>
2019-01-15 19:23:13 +0100

    Make welcome text easier to translate
    Integrate it in the source code and use tr() on it. It's going to be
    more tedious to update, but it removes the need to setup some special
    way to handle it, which will prove useful if I ever decide to use a
    service like Pootle or Weblate.

--- src/translations/nanonote_fr.ts
+++ src/translations/nanonote_fr.ts
@@ -49,6 +49,55 @@
         <source>Settings...</source>
         <translation>Configuration...</translation>
     </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="124"/>
+        <source>Welcome to Nanonote!
+
+Nanonote is a minimalist note taking application.
+
+It&apos;s meant for short-lived notes. Anything you type here is automatically saved on your disk.
+
+The only UI is the context menu, try it out!
+
+As you can see in the context menu, Nanonote has an &quot;Always on Top&quot; mode. This feature is handy to keep the window around.
+
+It also has a few handy editing features, like auto-bullet lists:
+
+- Try to move the cursor at the end of this line and press Enter
+- This works for
+    - nested lists
+    * and asterisks
+
+You can also open urls using Control + click. You can try clicking on this one for example: https://github.com/agateau/nanonote.
+
+Finally, you can indent selected lines with Tab or Ctrl+I and unindent them with Shift+Tab or Ctrl+U.
+
+That&apos;s all there is to say, now you can erase this text and start taking notes!
+</source>
+        <translation>Bienvenue dans Nanonote !
+
+Nanonote est une application de prise de note minimaliste.
+
+Elle a été conçue pour les notes éphémères. Tout ce que vous tapez ici est automatiquement enregistré sur votre disque.
+
+La seule interface utilisateur est le menu contextuel, essayez- le !
+
+Comme vous pouvez le voir dans le menu contextuel, Nanonote possède un mode &quot;Toujours visible&quot;. Ce mode est pratique pour garder l&apos;application à portée de main.
+
+L&apos;application dispose de quelques fonctionnalités d&apos;édition pratiques, comme les listes à puces automatique :
+
+- Placez le curseur à la fin de cette ligne et appuyez sur Entrée
+- Ça fonctionne pour
+    - les listes imbriquées
+    * et les astérisques
+
+Vous pouvez également ouvrir des urls en utilisant Control + clic. Vous pouvez essayer de cliquer sur celle-ci par exemple : https://github.com/agateau/nanonote.
+
+Enfin, vous pouvez indenter les lignes sélectionnées avec Tab ou Ctrl + I et les désindenter avec Shift+Tab ou Ctrl+U.
+
+C&apos;est tout ce qu&apos;il y a dire, maintenant vous pouvez effacer ce texte et commencer à prendre des notes !
+</translation>
+    </message>
 </context>
 <context>
     <name>SettingsDialog</name>
