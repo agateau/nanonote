c8c6093c068267b5bbc151a9e49da095a3d887f5
Aurelien Gateau <mail@agateau.com>
2019-01-15 19:30:25 +0100

    Add skeleton for Spanish translation

---
+++ src/translations/nanonote_es.ts
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>IndentExtension</name>
+    <message>
+        <location filename="../IndentExtension.cpp" line="51"/>
+        <source>Indent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../IndentExtension.cpp" line="58"/>
+        <source>Unindent</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LinkExtension</name>
+    <message>
+        <location filename="../LinkExtension.cpp" line="27"/>
+        <source>Copy link address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../LinkExtension.cpp" line="32"/>
+        <source>Open link</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../MainWindow.cpp" line="91"/>
+        <source>Increase Font Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="94"/>
+        <source>Decrease Font Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="103"/>
+        <source>Always on Top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="108"/>
+        <source>Settings...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="124"/>
+        <source>Welcome to Nanonote!
+
+Nanonote is a minimalist note taking application.
+
+It&apos;s meant for short-lived notes. Anything you type here is automatically saved on your disk.
+
+The only UI is the context menu, try it out!
+
+As you can see in the context menu, Nanonote has an &quot;Always on Top&quot; mode. This feature is handy to keep the window around.
+
+It also has a few handy editing features, like auto-bullet lists:
+
+- Try to move the cursor at the end of this line and press Enter
+- This works for
+    - nested lists
+    * and asterisks
+
+You can also open urls using Control + click. You can try clicking on this one for example: https://github.com/agateau/nanonote.
+
+Finally, you can indent selected lines with Tab or Ctrl+I and unindent them with Shift+Tab or Ctrl+U.
+
+That&apos;s all there is to say, now you can erase this text and start taking notes!
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../SettingsDialog.ui" line="14"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="24"/>
+        <source>Appearance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="30"/>
+        <source>Font family:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="42"/>
+        <source>Size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="68"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.cpp" line="41"/>
+        <source>&lt;h2&gt;Nanonote %1&lt;/h2&gt;&lt;p&gt;A minimalist note taking application.&lt;br&gt;%2&lt;/p&gt;&lt;p&gt;Your notes are stored in %3.&lt;/p&gt;</source>
+        <comment>%1=version %2=projectLink %3=noteLink</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
