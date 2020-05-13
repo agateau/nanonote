a22a83e161e27094e552fcae764832676f9d090c
Aurelien Gateau <mail@agateau.com>
2020-05-13 23:09:20 +0200

    Split dependency install into one script per OS
    It is easier to read and hopefully easier to maintain this way.

---
+++ ci/lib/linux-dependencies.sh
@@ -0,0 +1,28 @@
+main() {
+    echo_title "Installing Linux packages"
+    if has_command apt-get ; then
+        $RUN_AS_ROOT apt-get update
+        # file is needed by dpkg to generate shlib dependencies
+        $RUN_AS_ROOT apt-get install -y --no-install-recommends \
+            clang-format \
+            dpkg-dev \
+            extra-cmake-modules \
+            file \
+            g++ \
+            git \
+            make \
+            python3 \
+            python3-pip \
+            python3-setuptools \
+            qtbase5-dev \
+            qttools5-dev \
+            rpm \
+            xvfb
+    else
+        die "Sorry, I don't know how to install the required packages on your distribution."
+    fi
+
+    setup_python_cmd
+    install_cmake
+    install_qpropgen_dependencies
+}
