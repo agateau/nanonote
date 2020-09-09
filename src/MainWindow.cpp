9b324837b52cecd9fac2e1c2705056ebd333ea71
Aurelien Gateau <mail@agateau.com>
2020-09-09 08:44:33 +0200

    Group font actions in menu

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -40,9 +40,11 @@ MainWindowExtension::MainWindowExtension(MainWindow* window)
 
 void MainWindowExtension::aboutToShowContextMenu(QMenu* menu, const QPoint&) {
     menu->addAction(mWindow->mSearchAction);
+    menu->addSeparator();
     menu->addAction(mWindow->mIncreaseFontAction);
     menu->addAction(mWindow->mDecreaseFontAction);
     menu->addAction(mWindow->mResetFontAction);
+    menu->addSeparator();
     menu->addAction(mWindow->mAlwaysOnTopAction);
     menu->addSeparator();
     menu->addAction(mWindow->mSettingsAction);
