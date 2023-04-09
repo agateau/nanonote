ed10fc89dbdb336d8b5827c6c4da8be32b4832fd
Amerey <info@amerey.eu>
2023-04-09 16:39:33 +0200

    czech translation

---
+++ src/translations/nanonote_cs.ts
@@ -0,0 +1,290 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs">
+<context>
+    <name>IndentExtension</name>
+    <message>
+        <location filename="../IndentExtension.cpp" line="55"/>
+        <source>Indent</source>
+        <translation>Odsadit</translation>
+    </message>
+    <message>
+        <location filename="../IndentExtension.cpp" line="60"/>
+        <source>Unindent</source>
+        <translation>Zrušit odsazení</translation>
+    </message>
+</context>
+<context>
+    <name>LinkExtension</name>
+    <message>
+        <location filename="../LinkExtension.cpp" line="18"/>
+        <source>Go to link</source>
+        <translation>Přejít na odkaz</translation>
+    </message>
+    <message>
+        <location filename="../LinkExtension.cpp" line="30"/>
+        <source>Copy link address</source>
+        <translation>Kopírovat adresu odkazu</translation>
+    </message>
+    <message>
+        <source>Open link</source>
+        <translation type="vanished">Open link</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../MainWindow.cpp" line="114"/>
+        <source>Increase Font Size</source>
+        <translation>Zvětšit velikost písma</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="117"/>
+        <source>Decrease Font Size</source>
+        <translation>Zmenšit velikost písma</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="120"/>
+        <source>Reset Font Size</source>
+        <translation>Obnovit velikost písma</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="131"/>
+        <source>Always on Top</source>
+        <translation>Vždy nahoře</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="136"/>
+        <source>Settings | About...</source>
+        <translation>Nastavení | O aplikaci...</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="141"/>
+        <source>Find</source>
+        <translation>Najít</translation>
+    </message>
+    <message>
+        <location filename="../MainWindow.cpp" line="161"/>
+        <source>Welcome to Nanonote!
+
+Nanonote is a minimalist note taking application.
+
+Anything you type here is automatically saved on your disk.
+
+The only UI is the context menu, try it out!
+
+As you can see in the context menu, Nanonote has an &quot;Always on Top&quot; mode. This feature is handy to keep the window around.
+
+It also has a few handy editing features, like auto-bullet lists:
+
+- Try to move the cursor at the end of this line and press Enter
+- This works for
+    - nested lists
+* asterisks
+- [ ] checkable list entries (checkboxes can be toggled with Ctrl+Enter or Ctrl+click)
+
+You can open URLs using Ctrl+click or Ctrl+G while your cursor is inside an URL.
+Try clicking on this one for example, to learn more tricks:
+
+https://github.com/agateau/nanonote/blob/master/docs/tips.md
+
+That&apos;s all there is to say, now you can erase this text and start taking notes!
+</source>
+        <translation>Vítejte v Nanonote!
+
+Nanonote je minimalistická aplikace pro psaní poznámek.
+
+Vše, co zde zadáte, se automaticky uloží na váš disk.
+
+Jediné uživatelské rozhraní je kontextové menu, vyzkoušejte to!
+
+Jak můžete vidět v kontextové nabídce, Nanonote má režim „Vždy nahoře“. Tato funkce je užitečná pro udržení okna.
+
+Má také několik užitečných funkcí pro úpravy, například seznamy automatických odrážek:
+
+- Pokuste se přesunout kurzor na konec tohoto řádku a stiskněte Enter
+- Toto funguje pro
+     - vnořené seznamy
+* hvězdičky
+- [ ] zaškrtnutelné položky seznamu (zaškrtávací políčka lze přepínat pomocí Ctrl+Enter nebo Ctrl+kliknutí)
+
+Adresy URL můžete otevřít pomocí Ctrl+kliknutí nebo Ctrl+G, když je kurzor uvnitř adresy URL.
+Zkuste kliknout například na tento a dozvíte se další triky:
+
+https://github.com/agateau/nanonote/blob/master/docs/tips.md
+
+To je vše, co je třeba říci, nyní můžete tento text vymazat a začít si dělat poznámky!</translation>
+    </message>
+    <message>
+        <source>Welcome to Nanonote!
+
+Nanonote is a minimalist note taking application.
+
+It&apos;s meant for short-lived notes. Anything you type here is automatically saved on your disk.
+
+The only UI is the context menu, try it out!
+
+As you can see in the context menu, Nanonote has an &quot;Always on Top&quot; mode. This feature is handy to keep the window around.
+
+It also has a few handy editing features, like auto-bullet lists:
+
+- Try to move the cursor at the end of this line and press Enter
+- This works for
+    - nested lists
+    * and asterisks
+
+You can also open urls using Control + click or Control + Enter while your cursor is inside a URL. You can try clicking on this one for example: https://github.com/agateau/nanonote.
+
+Finally, you can indent selected lines with Tab or Ctrl+I and unindent them with Shift+Tab or Ctrl+U.
+
+That&apos;s all there is to say, now you can erase this text and start taking notes!
+</source>
+        <translation type="vanished">Welcome to Nanonote!
+
+Nanonote is a minimalist note taking application.
+
+It&apos;s meant for short-lived notes. Anything you type here is automatically saved on your disk.
+
+The only UI is the context menu, try it out!
+
+As you can see in the context menu, Nanonote has an &quot;Always on Top&quot; mode. This feature is handy to keep the window around.
+
+It also has a few handy editing features, like auto-bullet lists:
+
+- Try to move the cursor at the end of this line and press Enter
+- This works for
+    - nested lists
+    * and asterisks
+
+You can also open urls using Control + click or Control + Enter while your cursor is inside a URL. You can try clicking on this one for example: https://github.com/agateau/nanonote.
+
+Finally, you can indent selected lines with Tab or Ctrl+I and unindent them with Shift+Tab or Ctrl+U.
+
+That&apos;s all there is to say, now you can erase this text and start taking notes!
+</translation>
+    </message>
+</context>
+<context>
+    <name>MoveLinesExtension</name>
+    <message>
+        <location filename="../MoveLinesExtension.cpp" line="12"/>
+        <source>Move selected lines up</source>
+        <translation>Posunout vybrané řádky nahoru</translation>
+    </message>
+    <message>
+        <location filename="../MoveLinesExtension.cpp" line="17"/>
+        <source>Move selected lines down</source>
+        <translation>Posunout vybrané řádky dolů</translation>
+    </message>
+</context>
+<context>
+    <name>SearchWidget</name>
+    <message>
+        <location filename="../SearchWidget.ui" line="37"/>
+        <source>Previous</source>
+        <translation>Předchozí</translation>
+    </message>
+    <message>
+        <location filename="../SearchWidget.ui" line="53"/>
+        <source>Next</source>
+        <translation>Další</translation>
+    </message>
+    <message>
+        <location filename="../SearchWidget.cpp" line="16"/>
+        <source>Close search bar</source>
+        <translation>Zavřít vyhledávání</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../SettingsDialog.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Nastavení</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="34"/>
+        <source>Configuration</source>
+        <translation>Konfigurace</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="40"/>
+        <source>Font family:</source>
+        <translation>Font:</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="55"/>
+        <source>Size:</source>
+        <translation>Velikost:</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="125"/>
+        <source>Your notes are stored here:</source>
+        <translation>Vaše poznámky jsou uloženy zde:</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.ui" line="133"/>
+        <source>About</source>
+        <translation>0 aplikaci</translation>
+    </message>
+    <message>
+        <source>&lt;h2&gt;Nanonote %1&lt;/h2&gt;
+&lt;p&gt;A minimalist note taking application.&lt;br&gt;
+&lt;a href=&apos;%2&apos;&gt;%2&lt;/a&gt;&lt;/p&gt;</source>
+        <comment>%1: version, %2: project url</comment>
+        <translation type="vanished">&lt;h2&gt;Nanonote %1&lt;/h2&gt;
+&lt;p&gt;A minimalist note taking application.&lt;br&gt;
+&lt;a href=&apos;%2&apos;&gt;%2&lt;/a&gt;&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.cpp" line="49"/>
+        <source>&lt;h2&gt;Nanonote %1&lt;/h2&gt;
+&lt;p&gt;A minimalist note taking application.&lt;/p&gt;
+&lt;p&gt;
+&amp;bull; Project page: &lt;a href=&apos;%2&apos;&gt;%2&lt;/a&gt;&lt;br&gt;
+&amp;bull; Tips and tricks: &lt;a href=&apos;%3&apos;&gt;%3&lt;/a&gt;
+&lt;/p&gt;
+</source>
+        <comment>%1: version, %2: project url, %3: tips and trick page url</comment>
+        <translation>&lt;h2&gt;Nanonote %1&lt;/h2&gt;
+&lt;p&gt;Minimalistická aplikace na psaní poznámek.&lt;/p&gt;
+&lt;p&gt;
+&amp;bull; Stránka projektu: &lt;a href=&apos;%2&apos;&gt;%2&lt;/a&gt;&lt;br&gt;
+&amp;bull; Tipy a triky: &lt;a href=&apos;%3&apos;&gt;%3&lt;/a&gt;
+&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../SettingsDialog.cpp" line="59"/>
+        <source>&lt;p&gt;Hi,&lt;/p&gt;
+&lt;p&gt;I hope you enjoy Nanonote!&lt;/p&gt;
+&lt;p&gt;If you do, it would be lovely if you could &lt;a href=&apos;%1&apos;&gt;support my work&lt;/a&gt; on free and open source software.&lt;/p&gt;
+&lt;p align=&quot;right&quot;&gt;― Aurélien&lt;/p&gt;</source>
+        <comment>%1: support url</comment>
+        <translation>&lt;p&gt;Dobrý den,&lt;/p&gt;
+&lt;p&gt;Doufám, že se vám bude Nanonote líbit!&lt;/p&gt;
+&lt;p&gt;Pokud ano, bylo by skvělé, kdybyste mohli &lt;a href=&apos;%1&apos;&gt;podpořit mou práci&lt;/a&gt; na bezplatném softwaru s otevřeným zdrojovým kódem.&lt;/p&gt;
+&lt;p align=&quot;right&quot;&gt;― Aurélien&lt;/p&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>TaskExtension</name>
+    <message>
+        <location filename="../TaskExtension.cpp" line="35"/>
+        <source>Insert/toggle task</source>
+        <translation>Vložit/přepnout úkol</translation>
+    </message>
+</context>
+<context>
+    <name>TextEdit</name>
+    <message>
+        <location filename="../TextEdit.cpp" line="53"/>
+        <source>Edit</source>
+        <translation>Upravit</translation>
+    </message>
+    <message>
+        <location filename="../TextEdit.cpp" line="59"/>
+        <source>View</source>
+        <translation>Zobrazit</translation>
+    </message>
+</context>
+</TS>
