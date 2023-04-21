8658d87e30d1c1bdd9edca8c792dac4da2220ff5
Heimen Stoffels <vistausss@fastmail.com>
2023-04-21 15:09:28 +0200

    Added Dutch translation

---
+++ src/translations/nanonote_nl.ts
@@ -0,0 +1,228 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>IndentExtension</name>
+    <message>
+        <location filename="src/IndentExtension.cpp" line="55"/>
+        <source>Indent</source>
+        <translation>Inspringen</translation>
+    </message>
+    <message>
+        <location filename="src/IndentExtension.cpp" line="60"/>
+        <source>Unindent</source>
+        <translation>Inspringing opheffen</translation>
+    </message>
+</context>
+<context>
+    <name>LinkExtension</name>
+    <message>
+        <location filename="src/LinkExtension.cpp" line="18"/>
+        <source>Go to link</source>
+        <translation>Link openen</translation>
+    </message>
+    <message>
+        <location filename="src/LinkExtension.cpp" line="30"/>
+        <source>Copy link address</source>
+        <translation>Linkadres kopiëren</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="src/MainWindow.cpp" line="114"/>
+        <source>Increase Font Size</source>
+        <translation>Tekst vergroten</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="117"/>
+        <source>Decrease Font Size</source>
+        <translation>Tekst verkleinen</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="120"/>
+        <source>Reset Font Size</source>
+        <translation>Standaard tekstgrootte</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="131"/>
+        <source>Always on Top</source>
+        <translation>Altijd op voorgrond</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="136"/>
+        <source>Settings | About...</source>
+        <translation>Instellingen | Over…</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="141"/>
+        <source>Find</source>
+        <translation>Zoeken</translation>
+    </message>
+    <message>
+        <location filename="src/MainWindow.cpp" line="161"/>
+        <source>Welcome to Nanonote!
+
+Nanonote is a minimalist note taking application.
+
+Anything you type here is automatically saved on your disk.
+
+The only UI is the context menu, try it out!
+
+As you can see in the context menu, Nanonote has an &quot;Always on Top&quot; mode. This feature is handy to keep the window around.
+
+It also has a few handy editing features, like auto-bullet lists:
+
+- Try to move the cursor at the end of this line and press Enter
+- This works for
+    - nested lists
+* asterisks
+- [ ] checkable list entries (checkboxes can be toggled with Ctrl+Enter or Ctrl+click)
+
+You can open URLs using Ctrl+click or Ctrl+G while your cursor is inside an URL.
+Try clicking on this one for example, to learn more tricks:
+
+https://github.com/agateau/nanonote/blob/master/docs/tips.md
+
+That&apos;s all there is to say, now you can erase this text and start taking notes!
+</source>
+        <translation>Welkom in Nanonote!
+
+Nanonote is een minimalistisch notitieprogramma.
+
+Alles wat u typt wordt automatisch opgeslagen.
+
+Het enige grafische aan het programma is het rechtermuisknopmenu.
+
+Zoals u kunt zien in het rechtermuisknopmenu, is er een functie ‘Altijd op voorgrond’. Zo heeft u het venster altijd bij de hand.
+
+Ook zijn er enkele handige functies, zoals automatisch genummerde lijsten:
+
+- Verplaats de cursor naar het einde van deze regel en druk op enter
+- Dit werkt bij
+    - ingesprongen lijsten
+* sterretjes
+- [ ] aankruisbare lijstitems (toon/verberg ze met Ctrl+Enter of Ctrl+klik)
+
+Open url&apos;s met Ctrl+klik of Ctrl+G als de cursor op een url staat.
+Probeer bijvoorbeeld deze url uit, waar u tips kunt lezen (Engels):
+
+https://github.com/agateau/nanonote/blob/master/docs/tips.md
+
+Meer valt er niet over te vertellen. Wis deze tekst en ga aan de slag!
+</translation>
+    </message>
+</context>
+<context>
+    <name>MoveLinesExtension</name>
+    <message>
+        <location filename="src/MoveLinesExtension.cpp" line="12"/>
+        <source>Move selected lines up</source>
+        <translation>Selectie omhoog verplaatsen</translation>
+    </message>
+    <message>
+        <location filename="src/MoveLinesExtension.cpp" line="17"/>
+        <source>Move selected lines down</source>
+        <translation>Selectie omlaag verplaatsen</translation>
+    </message>
+</context>
+<context>
+    <name>SearchWidget</name>
+    <message>
+        <location filename="src/SearchWidget.ui" line="37"/>
+        <source>Previous</source>
+        <translation>Vorige</translation>
+    </message>
+    <message>
+        <location filename="src/SearchWidget.ui" line="53"/>
+        <source>Next</source>
+        <translation>Volgende</translation>
+    </message>
+    <message>
+        <location filename="src/SearchWidget.cpp" line="16"/>
+        <source>Close search bar</source>
+        <translation>Zoekbalk sluiten</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="src/SettingsDialog.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <location filename="src/SettingsDialog.ui" line="34"/>
+        <source>Configuration</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <location filename="src/SettingsDialog.ui" line="40"/>
+        <source>Font family:</source>
+        <translation>Lettertype:</translation>
+    </message>
+    <message>
+        <location filename="src/SettingsDialog.ui" line="55"/>
+        <source>Size:</source>
+        <translation>Grootte:</translation>
+    </message>
+    <message>
+        <location filename="src/SettingsDialog.ui" line="125"/>
+        <source>Your notes are stored here:</source>
+        <translation>Uw notities worden opgeslagen in</translation>
+    </message>
+    <message>
+        <location filename="src/SettingsDialog.ui" line="133"/>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message>
+        <location filename="src/SettingsDialog.cpp" line="49"/>
+        <source>&lt;h2&gt;Nanonote %1&lt;/h2&gt;
+&lt;p&gt;A minimalist note taking application.&lt;/p&gt;
+&lt;p&gt;
+&amp;bull; Project page: &lt;a href=&apos;%2&apos;&gt;%2&lt;/a&gt;&lt;br&gt;
+&amp;bull; Tips and tricks: &lt;a href=&apos;%3&apos;&gt;%3&lt;/a&gt;
+&lt;/p&gt;
+</source>
+        <comment>%1: version, %2: project url, %3: tips and trick page url</comment>
+        <translation>&lt;h2&gt;Nanonote %1&lt;/h2&gt;
+&lt;p&gt;Een minimalistisch notitieprogramma.&lt;/p&gt;
+&lt;p&gt;
+&amp;bull; Website: &lt;a href=&apos;%2&apos;&gt;%2&lt;/a&gt;&lt;br&gt;
+&amp;bull; Tips (Engels): &lt;a href=&apos;%3&apos;&gt;%3&lt;/a&gt;
+&lt;/p&gt;
+</translation>
+    </message>
+    <message>
+        <location filename="src/SettingsDialog.cpp" line="59"/>
+        <source>&lt;p&gt;Hi,&lt;/p&gt;
+&lt;p&gt;I hope you enjoy Nanonote!&lt;/p&gt;
+&lt;p&gt;If you do, it would be lovely if you could &lt;a href=&apos;%1&apos;&gt;support my work&lt;/a&gt; on free and open source software.&lt;/p&gt;
+&lt;p align=&quot;right&quot;&gt;― Aurélien&lt;/p&gt;</source>
+        <comment>%1: support url</comment>
+        <translation>%1: ondersteuning</translation>
+    </message>
+</context>
+<context>
+    <name>TaskExtension</name>
+    <message>
+        <location filename="src/TaskExtension.cpp" line="35"/>
+        <source>Insert/toggle task</source>
+        <translation>Taak invoegen/omschakelen</translation>
+    </message>
+</context>
+<context>
+    <name>TextEdit</name>
+    <message>
+        <location filename="src/TextEdit.cpp" line="53"/>
+        <source>Edit</source>
+        <translation>Bewerken</translation>
+    </message>
+    <message>
+        <location filename="src/TextEdit.cpp" line="59"/>
+        <source>View</source>
+        <translation>Beeld</translation>
+    </message>
+</context>
+</TS>
